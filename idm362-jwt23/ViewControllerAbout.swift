030833b9080358236f06a726c3336946befd6c54
Jervis Thompson <jervo@mac.com>
2023-02-16 08:43:27 -0500

    Pops view

---
+++ idm362-jwt23/ViewControllerAbout.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerAbout.swift
+//  idm362-jwt23
+//
+//  Created by Jervis Thompson on 2/16/23.
+//
+
+import UIKit
+
+class ViewControllerAbout: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
