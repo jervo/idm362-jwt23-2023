823bd0a9dae761b0a6347637d9634f234d078354
Jervis Thompson <jervo@mac.com>
2023-02-28 07:14:10 -0500

    Completed

--- idm362-jwt23/ViewControllerSettings.swift
+++ idm362-jwt23/ViewControllerSettings.swift
@@ -6,24 +6,84 @@
 //
 
 import UIKit
+import AVFoundation
+
+var myAudioPlayerObj = AVAudioPlayer()
+
 
 class ViewControllerSettings: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    // Do any additional setup after loading the view.
+    let haloSound = Bundle.main.path(forResource: "sounds/halo1", ofType: "mp3")
+    
+    do {
+      myAudioPlayerObj = try
+      AVAudioPlayer(contentsOf: URL(fileURLWithPath: haloSound!))
+      myAudioPlayerObj.prepareToPlay()
+      print("Sound file is loaded and ready to ROCK!")
+    } catch {
+      print(error)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+  }
+    
+  @IBAction func toggleHalo(_ sender: Any) {
+    print("toggleHalo called")
+    if (myAudioPlayerObj.isPlaying) {
+      myAudioPlayerObj.stop()
+    } else {
+      myAudioPlayerObj.play()
     }
-    */
+  }
+  
+  @IBAction func showAlert(_ sender: Any) {
+    let myAlertObj = UIAlertController(title: "Delete ALL Records", message: "Are you Sure?", preferredStyle: .alert)
+    
+    myAlertObj.addAction(UIAlertAction(title: "CANCEL", style: .cancel, handler: {
+      (UIAlertAction) in
+      print("User Canceled")
+    }))
+      
+    myAlertObj.addAction(UIAlertAction(title: "YES", style: .default, handler: { (UIAlertAction) in
+      print("User wants to delete call function")
+    }))
+    
+    myAlertObj.view.tintColor = UIColor(named: "Prince")
+    
+    self.present(myAlertObj, animated: true)
+  }
+  
+  var toogleFlag: Bool = false
+  
+  @IBOutlet weak var peepBtn: UIButton!
+  
+  @IBAction func peepBtnFun(_ sender: Any) {
+    if (toogleFlag == true) {
+      peepBtn.setBackgroundImage(UIImage(named: "people"), for: .normal)
+      peepBtn.tintColor = UIColor(named: "AccentColor")
+      toogleFlag = false
+    } else {
+      peepBtn.setBackgroundImage(UIImage(named: "user_man_woman"), for: .normal)
+      peepBtn.tintColor = UIColor(named: "Prince")
+      toogleFlag = true
+    }
+    
+  }
+  
+  
+  
+  
+  /*
+  // MARK: - Navigation
+
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+      // Get the new view controller using segue.destination.
+      // Pass the selected object to the new view controller.
+  }
+  */
 
 }
